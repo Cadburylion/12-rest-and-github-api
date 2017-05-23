db562fa5409d1d992c0cc05c162a465417cc443b
RyleeAndrews <ryleeandrews73@gmail.com>
2017-05-23 14:56:37 -0700

    all todos finished now working on error handling

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -15,15 +15,14 @@ var app = app || {};
   // TODO: Remember that new Handlebars template? Let's compile it!
   // Save the result in this `render` variable.
 
-
+  let render = Handlebars.compile($('#repo-template').text());
   repoView.index = function() {
     ui();
-    let render = Handlebars.compile($('#repo-template').text());
+
     // The jQuery `append` method lets us append an entire array of HTML elements at once:
     $('#about ul').append(
       app.repos.with('name').map(render) // Want to filter by a different property other than name?
     );
-    return render(this);
   };
 
   module.repoView = repoView;
